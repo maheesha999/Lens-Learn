394194f07d867485272d4b2572e934dc5bc63fc7
Diluka Athukorala <dilukaathukorala@gmail.com>
2025-05-09 21:21:31 +0530

    Added fixed width
    Added fixed width

--- frontend/src/Pages/PostManagement/AllPost.js
+++ frontend/src/Pages/PostManagement/AllPost.js
@@ -831,10 +831,10 @@ function AllPost() {
                                       cursor: 'pointer',
                                       fontSize: '24px', // Further increased from 20px
                                       margin: '0 5px',
-                                      padding: '8px', // 
+                                      padding: '8px', // Further increased from 6px
                                       borderRadius: '50%',
                                       backgroundColor: 'rgba(66, 133, 244, 0.1)',
-                                      width: '40px', // Added fixed width
+                                      width: '40px', // 
                                       height: '40px', // Added fixed height
                                       display: 'flex',
                                       alignItems: 'center',
