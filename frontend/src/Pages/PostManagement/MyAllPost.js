3b0ef394c56f2aef26343f9d63b93de368629770
nesha-913 <neshadharmarathna@gmail.com>
2025-05-10 17:12:31 +0530

    all posts

--- frontend/src/Pages/PostManagement/MyAllPost.js
+++ frontend/src/Pages/PostManagement/MyAllPost.js
@@ -25,7 +25,7 @@ function MyAllPost() {
   const [followedUsers, setFollowedUsers] = useState([]); // State to track followed users
   const [newComment, setNewComment] = useState({}); // State for new comments
   const [editingComment, setEditingComment] = useState({}); // State for editing comments
-  const [searchQuery, setSearchQuery] = useState(''); // State for search query
+  const [searchQuery, setSearchQuery] = useState(''); // State for a search query
   const navigate = useNavigate();
   const loggedInUserID = localStorage.getItem('userID'); // Get the logged-in user's ID
 
